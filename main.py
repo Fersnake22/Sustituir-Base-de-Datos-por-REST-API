0f417cd1dae923a2ce38890f7641d3079501089d
Fersnake22 <fernando.montano@uip.edu.pa>
2021-12-19 22:03:40 -0500

    Modulo 10

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+
+app = Flask(__name__)
+app.config['MONGO_URI']="mongodb://localhost/sustituir"
+mongo=PyMongo(app)
+
+@app.route('/', methods=['POST'])
+
+def create_user():
+
+	palabra = request.json['palabra']
+	significado = request.json['significado']
+
+	if palabra and significado:
+		id=mongo.db.slangdic.insert(
+				{'palabra':palabra, 'significado':significado}
+			)
+		response = {
+			'id':str(id),
+			'palabra':palabra,
+			'significado':significado,
+		}
+		return response
+	else:
+		return not_found()
+
+	return {'message':'received'}
+
+@app.route("/",methods=['GET'])
+def get_users():
+	users = mongo.db.slangdic.find()
+	response=json_util.dumps(users)
+	return Response(response, mimetype='application/json')
+
+
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+	response = jsonify({
+		'message': 'Resource Not Found: ' + request.url,
+		'status': 404
+	})
+	return response
+
+
+
+
+
+if __name__ == "__main__":
+	app.run(port=5022,debug=True)
